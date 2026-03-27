532263731c6752311ddd959923b62e1e60156a49
Meiirim <zh.meyirim@gmail.com>
2026-03-27 10:52:34 +0500

    removed unnecessary tables

--- src/database/DatabaseInitializer.tsx
+++ src/database/DatabaseInitializer.tsx
@@ -4,17 +4,12 @@ import { View, ActivityIndicator } from "react-native";
 import AppText from "../../components/AppText";
 import { theme } from "../theme";
 
-import * as TagsQueries from "./queries/TagsQueries";
 import * as CategoriesQueries from "./queries/CategoriesQueries";
 import * as NotesQueries from "./queries/NotesQueries";
-import * as NotesTagsQueries from "./queries/NotesTagsQueries";
 import * as SetsQueries from "./queries/SetsQueries";
 import * as CardsQueries from "./queries/CardsQueries";
 import * as TestsQueries from "./queries/TestsQueries";
 import * as QuestionsQueries from "./queries/QuestionsQueries";
-import * as RepetitionsQueries from "./queries/RepetitionsQueries";
-import * as CardsRepetitionsQueries from "./queries/CardsRepetitions";
-import * as TestsRepetitionsQueries from "./queries/TestsRepetitions";
 import { seedDatabase } from "./Seed";
 
 type Props = {
@@ -32,18 +27,12 @@ export default function DatabaseInitializer({ onReady }: Props) {
             <SQLiteProvider
             databaseName="local.db"
             onInit={async (db) => {
-                await db.execAsync(TagsQueries.DROP_TABLE);
-                await db.execAsync(TagsQueries.CREATE_TABLE);
-
                 await db.execAsync(CategoriesQueries.DROP_TABLE);
                 await db.execAsync(CategoriesQueries.CREATE_TABLE);
                 await db.runAsync(CategoriesQueries.INSERT, ["Нет", "#ababab"]);
 
                 await db.execAsync(NotesQueries.DROP_TABLE);
                 await db.execAsync(NotesQueries.CREATE_TABLE);
-
-                await db.execAsync(NotesTagsQueries.DROP_TABLE);
-                await db.execAsync(NotesTagsQueries.CREATE_TABLE);
 
                 await db.execAsync(SetsQueries.DROP_TABLE);
                 await db.execAsync(SetsQueries.CREATE_TABLE);
@@ -56,15 +45,6 @@ export default function DatabaseInitializer({ onReady }: Props) {
 
                 await db.execAsync(QuestionsQueries.DROP_TABLE);
                 await db.execAsync(QuestionsQueries.CREATE_TABLE);
-
-                await db.execAsync(RepetitionsQueries.DROP_TABLE);
-                await db.execAsync(RepetitionsQueries.CREATE_TABLE);
-
-                await db.execAsync(CardsRepetitionsQueries.DROP_TABLE);
-                await db.execAsync(CardsRepetitionsQueries.CREATE_TABLE);
-
-                await db.execAsync(TestsRepetitionsQueries.DROP_TABLE);
-                await db.execAsync(TestsRepetitionsQueries.CREATE_TABLE);
 
                 await seedDatabase(db);
 
